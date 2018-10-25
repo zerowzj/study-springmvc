6645633cbb88d80879163d2252f1d3293f78d051
wangzhenjun <wangzhenjun@okay.cn>
2018-10-25 18:06:05 +0800

    更新代码

---
+++ src/test/java/com/company/project/DIObject.java
@@ -0,0 +1,34 @@
+package com.company.project;
+
+public class DIObject {
+
+    private String name;
+
+    private Integer age;
+
+    private boolean isGood;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public boolean isGood() {
+        return isGood;
+    }
+
+    public void setIsGood(boolean good) {
+        isGood = good;
+    }
+}
--- src/test/java/com/company/project/SpringTestBase.java
+++ src/test/java/com/company/project/SpringTestBase.java
@@ -9,4 +9,5 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class SpringTestBase {
 
 
+
 }
