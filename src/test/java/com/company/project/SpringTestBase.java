90e45577e9259cb73ea9edc30290b4c8013e46cf
wangzhenjun <wangzhenjun@okay.cn>
2018-10-25 15:59:43 +0800

    更新代码

---
+++ src/test/java/com/company/project/SpringTestBase.java
@@ -0,0 +1,12 @@
+package com.company.project;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/spring-context.xml")
+public class SpringTestBase {
+
+
+}
