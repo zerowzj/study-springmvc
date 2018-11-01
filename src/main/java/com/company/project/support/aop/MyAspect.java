dd8ac241c97d7347d3f0b05ff865cc978c30a16b
wangzhenjun <wangzhenjun@okay.cn>
2018-11-01 17:17:37 +0800

    更新代码

---
+++ src/main/java/com/company/project/support/aop/MyAspect.java
@@ -0,0 +1,19 @@
+package com.company.project.support.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+
+@Aspect
+@Order(1)
+public class MyAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyAspect.class);
+
+    @Before("execution(* com.company.project.service..*.*(..))")
+    public void before(){
+        LOGGER.info("12313123123123123123123");
+    }
+}
