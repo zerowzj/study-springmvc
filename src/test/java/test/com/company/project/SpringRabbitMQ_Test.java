340fd9ef1cda7efc7bb441df3f6131ec77608805
wangzhenjun <wangzhenjun@okay.cn>
2018-11-05 17:00:11 +0800

    更新代码

--- src/test/java/test/com/company/project/SpringRabbitMQ_Test.java
+++ src/test/java/test/com/company/project/SpringRabbitMQ_Test.java
@@ -2,6 +2,8 @@ package test.com.company.project;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -9,8 +11,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration({"classpath:spring/spring-context.xml", "classpath:spring/spring-dao.xml"})
 public class SpringRabbitMQ_Test {
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     @Test
     public void test() {
-
+        rabbitTemplate.convertAndSend("sddddddddddddfsdf");
     }
 }
