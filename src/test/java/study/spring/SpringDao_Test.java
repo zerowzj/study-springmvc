e1b31dacdf354bb528bd08ca40007e992f4967c7
wangzhenjun <wangzhenjun@okay.cn>
2018-12-14 18:19:02 +0800

    更新代码

--- src/test/java/study/spring/SpringDao_Test.java
+++ src/test/java/study/spring/SpringDao_Test.java
@@ -8,14 +8,16 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:spring/spring-context.xml", "classpath:spring/spring-dao.xml"})
+@ContextConfiguration({"classpath:spring/spring-context.xml",
+        "classpath:spring/spring-dao.xml",
+        "classpath:spring/spring-datasource.xml"})
 public class SpringDao_Test {
 
     @Autowired
     private UserBaseDao userBaseDao;
 
     @Test
-    public void test() {
+    public void get_test() {
         userBaseDao.get(65L);
     }
 }
