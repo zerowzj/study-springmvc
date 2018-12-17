b389368edd4a74115d410ac6238c595c21997656
wangzhenjun <wangzhenjun@okay.cn>
2018-12-17 14:01:49 +0800

    更新代码

--- src/main/java/com/company/project/service/UserServiceImpl.java
+++ src/main/java/com/company/project/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.company.project.service;
 import com.company.project.dao.useradmin.UserAdminDao;
 import com.company.project.dao.useradmin.UserAdminEO;
 import com.company.project.dao.userbase.UserBaseDao;
+import io.shardingsphere.api.HintManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -22,6 +23,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void checkInAdmin(UserAdminEO uaEO) {
+        HintManager hintManager = HintManager.getInstance();
+        hintManager.setMasterRouteOnly();
         System.out.println("name=" + name);
     }
 }
