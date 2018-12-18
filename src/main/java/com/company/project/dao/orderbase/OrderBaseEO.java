ebda852b6bc86a709b9e7f6883c621ac5cc11e9c
wangzhenjun <wangzhenjun@okay.cn>
2018-12-18 15:29:51 +0800

    更新代码

--- src/main/java/com/company/project/dao/orderbase/OrderBaseEO.java
+++ src/main/java/com/company/project/dao/orderbase/OrderBaseEO.java
@@ -4,4 +4,9 @@ import com.company.project.common.db.BaseEO;
 
 public class OrderBaseEO extends BaseEO {
 
+    /* 主键 */
+    private Long rbId;
+    /* 订单号 */
+    private Long rbOrderId;
+
 }
--- src/main/java/com/company/project/dao/orderitem/OrderItemEO.java
+++ src/main/java/com/company/project/dao/orderitem/OrderItemEO.java
@@ -4,4 +4,9 @@ import com.company.project.common.db.BaseEO;
 
 public class OrderItemEO extends BaseEO {
 
+    /* 主键 */
+    private Long riId;
+    /* 订单号 */
+    private Long riOrderId;
+
 }
