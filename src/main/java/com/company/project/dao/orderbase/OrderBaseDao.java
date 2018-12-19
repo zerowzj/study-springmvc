39e9296da4e53fced7512b43c9a855b18018ad9a
wangzhenjun <wangzhenjun@okay.cn>
2018-12-19 09:24:16 +0800

    更新代码

--- src/main/java/com/company/project/dao/orderbase/OrderBaseDao.java
+++ src/main/java/com/company/project/dao/orderbase/OrderBaseDao.java
@@ -3,6 +3,10 @@ package com.company.project.dao.orderbase;
 import com.company.project.common.db.BaseDao;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository("OrderBaseDao")
 public interface OrderBaseDao extends BaseDao<Long, OrderBaseEO> {
+
+    List getByOrderId(Long orderId);
 }
