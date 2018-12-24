de251fa25f90002868b431cc44079d39d64a6da3
wangzhenjun <wangzhenjun@okay.cn>
2018-12-24 16:52:53 +0800

    更新代码

--- src/main/java/com/company/project/dao/userbase/UserBaseDao.java
+++ src/main/java/com/company/project/dao/userbase/UserBaseDao.java
@@ -3,8 +3,12 @@ package com.company.project.dao.userbase;
 import com.company.project.common.db.BaseDao;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository("userBaseDao")
 public interface UserBaseDao extends BaseDao<Long, UserBaseEO> {
 
     UserBaseEO getByUserId(Long userId);
+
+    List<UserBaseEO> getLt(List<Long> ubIdLt, List<Long> userIdLt);
 }
