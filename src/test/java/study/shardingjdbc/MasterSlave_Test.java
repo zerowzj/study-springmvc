8f029775a7fd4a7f1c0aa0aad6d0df6c1a05a111
wangzhenjun <wangzhenjun@okay.cn>
2018-12-27 14:37:28 +0800

    更新代码

--- src/test/java/study/shardingjdbc/MasterSlave_Test.java
+++ src/test/java/study/shardingjdbc/MasterSlave_Test.java
@@ -33,8 +33,8 @@ public class MasterSlave_Test {
 
     @Test
     public void getByUserId_test() {
-        HintManager.getInstance().setMasterRouteOnly();
-        UserBaseEO ubEO = userBaseDao.getByUserId(1545634412870L);
+//        HintManager.getInstance().setMasterRouteOnly();
+        UserBaseEO ubEO = userBaseDao.getByUserId(1545819049828L);
         LOGGER.info("{}", ubEO.getUbLoginName());
     }
 
