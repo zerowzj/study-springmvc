2eaed609e2f7a6b07100de19a122f97919876c67
wangzhenjun <wangzhenjun@okay.cn>
2019-05-20 15:11:25 +0800

    更新代码

--- src/main/java/study/spring/support/aop/MyBeforeAdvice.java
+++ src/main/java/study/spring/support/aop/MyBeforeAdvice.java
@@ -1,13 +1,17 @@
 package study.spring.support.aop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.aop.MethodBeforeAdvice;
 
 import java.lang.reflect.Method;
 
 public class MyBeforeAdvice implements MethodBeforeAdvice {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyBeforeAdvice.class);
+
     @Override
     public void before(Method method, Object[] args, Object target) throws Throwable {
-        System.out.println("ffffffffffasasdfsadfasdfsadf");
+        LOGGER.info("ffffffffffasasdfsadfasdfsadf");
     }
 }
