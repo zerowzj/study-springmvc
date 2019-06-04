497aaa4ee9835c2e17a6307d94dd43d9ef42ec85
wangzhenjun <wangzhenjun@okay.cn>
2019-06-04 16:54:34 +0800

    更新代码

--- src/main/java/study/spring/support/interceptor/MyInterceptor.java
+++ src/main/java/study/spring/support/interceptor/MyInterceptor.java
@@ -15,18 +15,19 @@ public class MyInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
                              Object handler) throws Exception {
-        return false;
+        LOGGER.info("preHandlepreHandlepreHandlepreHandle");
+        return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response,
                            Object handler, ModelAndView modelAndView) throws Exception {
-
+        LOGGER.info("postHandlepostHandlepostHandle");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
                                 Object handler, Exception ex) throws Exception {
-
+        LOGGER.info("afterCompletionafterCompletionafterCompletion");
     }
 }
