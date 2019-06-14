110c32a54b55571d5125055d11bd067568eb4cbc
wangzhenjun <wangzhenjun@okay.cn>
2019-06-15 00:31:55 +0800

    更新代码

--- src/main/java/study/spring/web/databind/DataBindController.java
+++ src/main/java/study/spring/web/databind/DataBindController.java
@@ -3,7 +3,6 @@ package study.spring.web.databind;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import study.spring.support.context.SpringContext;
 
 @Controller
 @RequestMapping("/databind")
@@ -29,7 +27,7 @@ public class DataBindController {
 
     @RequestMapping("/request")
     public void request(@RequestParam(required = false) String name,
-                             @RequestHeader(value = "Request-Id", required = false) String requestId) {
+                        @RequestHeader(value = "Request-Id", required = false) String requestId) {
         LOGGER.info("name={}, request_id={}", name, requestId);
     }
 
@@ -40,7 +38,6 @@ public class DataBindController {
 
     @RequestMapping("/bind4")
     public void bind4(@RequestBody MultiValueMap<String, String> map) {
-        String[] str = SpringContext.getBeanNamesForType(HttpMessageConverter.class);
         LOGGER.info("name={}, age={}", map);
     }
 }
