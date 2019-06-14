29e30f41884e08e49c55d64ce8e08b87f2f822fd
wangzhenjun <wangzhenjun@okay.cn>
2019-06-14 22:18:46 +0800

    更新代码

--- src/main/java/study/spring/web/databind/DataBindController.java
+++ src/main/java/study/spring/web/databind/DataBindController.java
@@ -15,32 +15,32 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import study.spring.support.context.SpringContext;
 
 @Controller
-@RequestMapping("/bind")
+@RequestMapping("/databind")
 @ResponseBody
 public class DataBindController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DataBindController.class);
 
-    @RequestMapping("/{id}/{name}")
-    public void bind1(String id1,
-                      @PathVariable String name) {
+    @RequestMapping("/path/{id}/{name}")
+    public void pathVariable(String id1,
+                             @PathVariable String name) {
         LOGGER.info("id={},name={}", id1, name);
     }
 
-    @RequestMapping("/bind2")
-    public void bind2(@RequestParam(required = false) String name,
-                      @RequestHeader(value = "Request-Id", required = false) String requestId) {
+    @RequestMapping("/request")
+    public void request(@RequestParam(required = false) String name,
+                             @RequestHeader(value = "Request-Id", required = false) String requestId) {
         LOGGER.info("name={}, request_id={}", name, requestId);
     }
 
-    @RequestMapping("/bind3")
-    public void bind3(FormBean form) {
+    @RequestMapping("/bean")
+    public void bean(FormBean form) {
         LOGGER.info("name={}, age={}", form.getName(), form.getAge());
     }
 
     @RequestMapping("/bind4")
-    public void bind4(@RequestBody MultiValueMap<String,String> map) {
-        String[] str  = SpringContext.getBeanNamesForType(HttpMessageConverter.class);
+    public void bind4(@RequestBody MultiValueMap<String, String> map) {
+        String[] str = SpringContext.getBeanNamesForType(HttpMessageConverter.class);
         LOGGER.info("name={}, age={}", map);
     }
 }
