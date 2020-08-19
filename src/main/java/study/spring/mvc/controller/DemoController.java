f2e46a4b50e3cf289231c17df6f58aa037c549d8
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-08-19 15:32:28 +0800

    更新代码

---
+++ src/main/java/study/spring/mvc/controller/DemoController.java
@@ -0,0 +1,26 @@
+package study.spring.mvc.controller;
+
+import com.google.common.collect.Maps;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+@Controller
+public class DemoController {
+
+    @RequestMapping(value = "/sayHi")
+    @ResponseBody
+    public String sayHi() {
+        return "hi";
+    }
+
+    @RequestMapping(value = "/sayBye")
+    @ResponseBody
+    public User sayBye() {
+        Map<String, Object> data = Maps.newHashMap();
+        data.put("code", "000");
+        return new User();
+    }
+}
---
+++ src/main/java/study/spring/mvc/controller/User.java
@@ -0,0 +1,13 @@
+package study.spring.mvc.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class User {
+
+    private Long id = 90001L;
+
+    private String name = "wzj";
+}
