3252219ea4d085dc70668b14a5637915e3266bdf
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-08-25 10:00:36 +0800

    更新代码

--- src/main/java/study/spring/mvc/controller/RequestMappingController.java
+++ src/main/java/study/spring/mvc/controller/RequestMappingController.java
@@ -1,6 +1,7 @@
 package study.spring.mvc.controller;
 
 import com.google.common.collect.Maps;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
---
+++ src/test/java/study/spring/namespace/JettyServer.java
@@ -0,0 +1,25 @@
+package study.spring.namespace;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class JettyServer {
+
+    private static final String WEB_APP_LOCATION = "D:\\project\\study\\study-spring\\src\\main\\webapp";
+
+    private static final String CONTEXT_PATH = "/";
+
+    private static final int PORT = 8080;
+
+    public static void main(String[] args) {
+        try {
+            Server server = new Server(PORT);
+            WebAppContext webapp = new WebAppContext(WEB_APP_LOCATION, CONTEXT_PATH);
+            server.setHandler(webapp);
+            server.start();
+            server.join();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
