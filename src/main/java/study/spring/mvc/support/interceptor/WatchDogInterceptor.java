55398708664dfa5f77fd54376093a0252125c306
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-08-26 19:20:11 +0800

    更新代码

--- src/main/java/study/spring/mvc/support/interceptor/WatchDogInterceptor.java
+++ src/main/java/study/spring/mvc/support/interceptor/WatchDogInterceptor.java
@@ -13,19 +13,19 @@ public class WatchDogInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
                              Object handler) throws Exception {
-        log.info("preHandle");
+        log.info(">>>>>> preHandle");
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response,
                            Object handler, ModelAndView modelAndView) throws Exception {
-        log.info("postHandle");
+        log.info(">>>>>> postHandle");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
                                 Object handler, Exception ex) throws Exception {
-        log.info("afterCompletion");
+        log.info(">>>>>> afterCompletion");
     }
 }
